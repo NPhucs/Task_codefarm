4b530cd7e503298e3798e4c58b6039cafa2c95b5
NPhucs <phucnguyen11204@gmail.com>
2025-08-11 20:27:22 +0700

    task10 bai1

---
+++ TASK10/BT/ex01.js
@@ -0,0 +1,14 @@
+function convertArrayToObject(array) {
+  const result = {};
+
+  for (let item of array) {
+    const [key, value] = item.split(":");
+
+    result[key] = value;
+  }
+
+  return result;
+}
+
+const arrayString = ["name:John", "age:30", "city:NY"];
+console.log(convertArrayToObject(arrayString)); // { name: 'John', age: '30', city: 'NY' }
